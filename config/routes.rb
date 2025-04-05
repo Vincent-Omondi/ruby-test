427eaab271de39ec4f4968b1137d9f3930af751d
Vincent-Omondi <vincentomondi251@gmail.com>
2025-04-06 00:36:30 +0300

    Add redirects for authentication paths and define placeholder routes for menu items in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,25 @@ Rails.application.routes.draw do
   get 'locations/edit'
   get 'locations/update'
   get 'locations/destroy'
-  devise_for :users
+  
+  # Add redirects for common authentication paths
+  get '/login', to: redirect('/users/sign_in')
+  get '/signup', to: redirect('/users/sign_up')
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
+  get 'protected', to: 'home#protected'
+  
+  # Placeholder routes for menu items
+  get 'people', to: 'home#index'
+  get 'projects', to: 'home#index'
+  get 'places', to: 'home#index'
+  get 'ubuntu', to: 'home#index'
+  get 'about', to: 'home#index'
+  get 'blog', to: 'home#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
